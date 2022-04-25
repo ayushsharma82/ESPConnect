cb024ec91b90c0def00b225a2571f21337c36833
Ayush Sharma <asrocks5@gmail.com>
2022-04-25 19:35:49 +0530

    Delete build_platformio.sh

--- build_platformio.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-pio lib -g install "ESP Async WebServer@1.2.3"
-
-pio ci --lib=. --board esp32dev "examples/AutoConnect"
-pio ci --lib=. --board esp12e "examples/AutoConnect"

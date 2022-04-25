f8f77240668289fa4cd40ecb555802e2b542daa9
Ayush Sharma <asrocks5@gmail.com>
2022-04-25 17:01:44 +0530

    Create dep-install.sh

---
+++ .github/scripts/dep-install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git clone https://github.com/me-no-dev/ESPAsyncWebServer.git ESPAsyncWebServer
+git clone https://github.com/me-no-dev/ESPAsyncTCP.git ESPAsyncTCP
+git clone https://github.com/me-no-dev/AsyncTCP.git AsyncTCP
+
